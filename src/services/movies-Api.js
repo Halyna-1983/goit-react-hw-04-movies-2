cb05e6354886dcd03af3cfa6c442b7e44e710cfa
Halyna-1983 <72556196+Halyna-1983@users.noreply.github.com>
2021-08-03 01:19:27 +0300

    search

--- src/services/movies-Api.js
+++ src/services/movies-Api.js
@@ -16,15 +16,17 @@ const fetchById = async (id) => {
 
 const fetchCast = async (id) => {
 	const response = await Axios.get(`${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`)
-    // console.log(response.data.cast)
-	return response.data.cast;
+   	return response.data.cast;
 }
 
 const fetchReviews = async (id) => {
 	const response = await Axios.get(`${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}`)
-    // console.log(response.data.reviews)
-	return response.data.results;
+    return response.data.results;
 }
 
+const searchMovies = async (query) => {
+	const response = await Axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`)
+	return response.data.results;
+}
 // eslint-disable-next-line
-export default {fetchMovies, fetchById, fetchCast, fetchReviews};
+export default {fetchMovies, fetchById, fetchCast, fetchReviews, searchMovies};
--- src/views/SearchView/SearchView.js
+++ src/views/SearchView/SearchView.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import SearchForm from "../../component/SearchForm";
+import Api from "../../services/movies-Api";
+import MoviesList from "../../component/MoviesList";
 
 
 class SearchView extends Component {
-    state = {  }
+    state = { 
+        searchQuery: '',
+        movies: [],
+     }
+
+     componentDidMount() {
+         if (this.props.location?.state?.searchQuery) {
+             this.setState({ searchQuery: this.props.location?.state?.searchQuery });
+         }
+     }
+
+     componentDidUpdate(prevProps, prevState) {
+         if (prevState.searchQuery !== this.state.searchQuery) {
+             this.fetchMovie();
+         }
+     }
+
+     onChangeQuery = query => {
+     this.setState({ searchQuery: query });
+     this.props.history.push({
+         pathname: this.props.location.pathname,
+         search: `query=${query}`,
+         state: {searchQuery: query}
+    })
+    }
+
+     fetchMovie = async() => {
+         const movies = await Api.searchMovies(this.state.searchQuery)
+         this.setState({ movies });
+     }
+
     render() { 
         return ( 
             <div>
-                <h1> Search </h1>
-                <input type="text" />
+                <h1> Search movies</h1>
+                <SearchForm onSubmit={this.onChangeQuery}/>
+                <MoviesList movies={this.state.movies}/>
             </div>
          );
     }
